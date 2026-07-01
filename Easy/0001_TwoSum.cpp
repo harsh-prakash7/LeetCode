25adbab12118bb690c16cc6bdab49b69aa68aa74
harsh-prakash7 <harshprakash31@gmail.com>
2026-07-01 23:55:19 +0530

    Fix Code

--- Easy/0001_TwoSum.cpp
+++ Easy/0001_TwoSum.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <vector>
-
+#include<unordered_map>
 using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-
-        for(int i = 0; i < nums.size(); i++)
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++)
         {
-            for(int j = i + 1; j < nums.size(); j++)
+            int complement =target -nums[i];
+            if(mp.find(complement)!= mp.end())
             {
-                if(nums[i] + nums[j] == target)
-                {
-                    return {i, j};
-                }
+                return{mp[complement], i};
             }
+            mp[nums[i]]= i;
         }
-
         return {};
     }
 };
